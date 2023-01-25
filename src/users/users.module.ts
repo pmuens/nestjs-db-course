21d2fa5a0c446ad57ab8fe0f59f7c6470dbfa7e2
Philipp Muens <philipp@muens.io>
2023-01-25 12:30:53 +0100

    Generate Users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './controllers/users/users.controller';
 
-@Module({})
+@Module({
+  controllers: [UsersController]
+})
 export class UsersModule {}
